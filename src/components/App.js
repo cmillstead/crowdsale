574e1a4d83807b7c2948987a5ab0fc3d47807af2
Cevin Millstead <cevinm@gmail.com>
2023-07-31 18:47:01 -0700

    load accounts

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+
+
 import Navigation from './Navigation';
+import Info from './Info';
 
 function App() {
-  return (
-    <Container>
-      <Navigation />
-    </Container>
-  );
+
+    const [account, setAccount] = useState(null)
+
+    const loadBlockchainData = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0]);
+        setAccount(account);
+    };
+
+    useEffect(() => {
+        if (window.ethereum) {
+            loadBlockchainData();
+        } else {
+            window.alert('Please install MetaMask');
+        }
+    });
+
+    return (
+        <Container>
+            <Navigation />
+            <hr />
+            {account && (
+                <Info account={account} />
+            )}
+        </Container>
+    );
 }
 
 export default App;
